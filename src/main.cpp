8dbd569709941923d1d762e748ddf2fbad5a1250
Amir M. Jahangirzadeh <ajahangirzadeh81@gmail.com>
2021-07-17 18:26:19 +0430

    getopt Added To The Program

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,59 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <getopt.h>
 #include "libs/hexlib.hpp"
 #include "libs/simpleIni.h"
-
-#define pass (void)0
 
 using namespace std;
 
 void checker(string, string, string , string);
+int banner(string);
 
 int main(int argc, char **argv)
 {
-	//check argc
-	if (argc != 3) {
-		cout << " Usage\n\t"
-		    << argv[0] << " <file> " << " <ini_file> ";
-		exit(1);
-	}
-
 	char ch;
+	char *fvalue = NULL;
+	char *ivalue = NULL;
+	int opt,iflag,fflag;
 	string hexvalues;
 	CSimpleIniA ini;
 	CSimpleIniA::TNamesDepend sections;
-	SI_Error rc = ini.LoadFile(argv[2]);
-	ifstream fin(argv[1], ios::binary);
+	
+	while((opt = getopt(argc, argv, "f:i:")) != -1)
+	{
+		switch(opt)
+		{
+			case 'i':
+				iflag = 1;
+				ivalue = optarg;
+				break;
+			case 'f':
+				fflag = 1;
+				fvalue = optarg;
+				break;
+			default:
+				banner(argv[0]);
 
+		}
+	}
+
+	if(iflag != 1 || fflag != 1) banner(argv[0]);
+
+	ifstream fin(fvalue, ios::binary);
+	SI_Error rc = ini.LoadFile(ivalue);
+	
 	// Open Files Error Handling 
 	if (!fin) {
-		cout << "Error : Cannot Open " << argv[1] << endl;
+		cout << "Error : Cannot Open " << fvalue << endl;
 		exit(1);
 	}
 
 	if (rc < 0) {
-		cout << "Error : Cannot Open " << argv[2] << endl;
+		cout << "Error : Cannot Open " << ivalue << endl;
 		exit(1);
 	}
+	
 	// Getting file bytes into hexvalues var
 	for (int i = 0; !fin.eof(); i++) {
 		fin.get(ch);
@@ -79,3 +97,13 @@ void checker(string pattrn, string hexvalue, string section,string ep_verify)
 	}
 
 }
+
+int banner(string argzero)
+{
+	cout << " Usage:" << argzero << "[OPTION]....[OPTION].....\
+	\n Binary FILE Type Detector By Hexadecimal Signatures.\
+  	\n\t-f,                   Load The Target File \
+  	\n\t-i                    Load The INI File" << endl;
+	exit(1);
+}
+
